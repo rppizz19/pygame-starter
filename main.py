fa797dc3fcae7364b1dfddc882d43384f4597daa
Ronald Pizzini <rppizz19>
2020-02-17 10:32:37 -0500

    changed images

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import pygame # import library
-pygame.init()
-
+pygame.init() 
 # Create the window
 win = pygame.display.set_mode((800, 600))
+img = pygame.image.load('assets/Top_Down_Survivor/knife/idle/survivor-idle_knife_0.png').convert_alpha()
+font = pygame.font.SysFont ("arial", 72)
+text = font.render ("Welcome", True,  (255, 255, 255))
 
+
+  
+  # Create
 run = True
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
 
-# Game code starts here ---------------------
-  win.fill((0, 0, 0))
 
-  # Draw a rectangle
-  pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
   
+ 
+        
+    
+  
+
+
+win.fill((100, 50, 20))
+win.blit(img, (400, 300))
+win.blit(text, (400, 300))
+
+  # Draw a rectangle  
   #Update the display
-  pygame.display.update()
+pygame.display.update()
 
 print("Ending game")
 pygame.quit()
